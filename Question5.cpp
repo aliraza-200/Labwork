46d2ea7b6dcd64f259bdaef1a0afd0e2c443e7a1
Ali Raza <ch.alirazaarain20@gmail.com>
2026-03-10 22:19:14 +0500

    Create Question5.cpp

---
+++ Question5.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+class GameCharacter {
+private:
+    string name;
+    int health;
+    int maxHealth;
+
+public:
+    GameCharacter(string n, int m) {
+        name = n;
+        maxHealth = m;
+        health = maxHealth;
+    }
+
+    string getName() {
+        return name;
+    }
+
+    int getHealth() {
+        return health;
+    }
+
+    void takeDamage(int damage) {
+        health -= damage;
+
+        if (health <= 0) {
+            health = 0;
+            cout << "Character has fainted." << endl;
+        }
+    }
+
+    void heal(int amount) {
+        health += amount;
+
+        if (health > maxHealth) {
+            health = maxHealth;
+        }
+    }
+};
+
+int main() {
+    GameCharacter hero("Arthur", 100);
+
+    hero.takeDamage(120);
+    hero.heal(50);
+    hero.heal(200);
+
+    cout << "Final Health: " << hero.getHealth() << endl;
+}
